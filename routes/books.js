cd2d13ed1c675871ab507d35be1d77684c27333b
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2023-05-31 19:53:40 +0200

    add links to view all books and add a new book page.

--- routes/books.js
+++ routes/books.js
@@ -3,12 +3,18 @@ const Book = require("../models/book");
 const router = express.Router();
 
 // All books route
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  res.send("All Books");
+});
 
 // Display new book route
-router.get("/new", (req, res) => {});
+router.get("/new", (req, res) => {
+  res.send("New book");
+});
 
 // Create new book
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+  res.send("create book");
+});
 
 module.exports = router;
