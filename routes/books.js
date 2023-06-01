4e720241d144032537918db0a2a9c485c04fbe96
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2023-06-01 23:24:37 +0200

    create front end of adding new book. move to creating the backend.

--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Book = require("../models/book");
+const Author = require("../models/author");
 const router = express.Router();
 
 // All books route
@@ -8,8 +9,17 @@ router.get("/", async (req, res) => {
 });
 
 // Display new book route
-router.get("/new", (req, res) => {
-  res.send("New book");
+router.get("/new", async (req, res) => {
+  try {
+    const authors = await Author.find({});
+    const book = new Book();
+    res.render("books/new", {
+      authors: authors,
+      book: book,
+    });
+  } catch {
+    res.redirect("/books");
+  }
 });
 
 // Create new book
