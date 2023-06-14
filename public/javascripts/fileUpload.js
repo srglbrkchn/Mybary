8aed4c57b61e378165f4375ce663eb607f51e9ab
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2023-06-14 12:13:45 +0200

    set up filepond to load book cover pictures as a data string inside db, instead of the server.

---
+++ public/javascripts/fileUpload.js
@@ -0,0 +1,15 @@
+// register plugins
+FilePond.registerPlugin(
+  FilePondPluginImagePreview,
+  FilePondPluginImageResize,
+  FilePondPluginFileEncode
+);
+
+FilePond.setOptions({
+  stylePanelAspectRatio: 150 / 100,
+  imageResizeTargetWidth: 100,
+  imageResizeTargetHeight: 150,
+});
+
+// parsing all of our file inputs into filepond objects
+FilePond.parse(document.body);
